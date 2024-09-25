f4b62cabd53751d30440b0528b4c7d75f9ff063f
Aaron <trapp.root@gmail.com>
2024-09-24 17:15:45 -0700

    Dropdown with styling and page loading done

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-(function () {
-  const menuBtn = document.querySelector('.menu');
-  const menuItemsDiv = document.querySelector('.menu-items');
-  
-  menuBtn.addEventListener('click', () => {
-    const isHidden = menuItemsDiv.classList.value.includes('hidden');
-    
-    if (isHidden) {
-      menuItemsDiv.classList.remove('hidden');
-    } else {
-      menuItemsDiv.classList.add('hidden');
-    }	
-  });
-})();
+const menuBtn = document.querySelector('.menu-btn');
+const homeBtn = document.querySelector('.home-btn');
+const aboutBtn = document.querySelector('.about-btn');
+const contactBtn = document.querySelector('.contact-btn');
 
+const menuItemsDiv = document.querySelector('.menu-items');
+const contentDiv = document.querySelector('.content-container')
 
+const toggleMenuVisibility = () => {
+	const isHidden = menuItemsDiv.classList.value.includes('hidden');
+  if (isHidden) {
+		menuItemsDiv.classList.remove('hidden');
+	} else {
+		menuItemsDiv.classList.add('hidden');
+	}
+};
+
+const renderPage = (e) => {
+  toggleMenuVisibility();
+  const page = e.currentTarget.innerText;
+  contentDiv.innerText = page;
+}
+
+homeBtn.addEventListener('click', renderPage)
+aboutBtn.addEventListener('click', renderPage)
+contactBtn.addEventListener('click', renderPage)
+
+menuBtn.addEventListener('click', toggleMenuVisibility)
